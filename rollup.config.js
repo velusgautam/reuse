bfe64d339ea9f54402cbcaba17e8b640b1339c1e
Velu Gautam <velu.gautam@comeon.com>
2019-08-22 15:01:21 +0200

    :hammer: Some refactoring

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,24 @@
 import { terser } from "rollup-plugin-terser";
+import { version } from "./package.json";
 
 export default {
   input: "./src/main.js",
-  plugins: [terser()],
   output: {
-    compact: true,
     file: "dist/main.js",
-    format: "esm"
-  }
+    format: "esm",
+    banner: "/* reuse Library version: " + version + " */",
+    footer: "/* follow me on Github! https://github.com/velusgautam */"
+  },
+  plugins: [
+    terser({
+      output: {
+        comments: function(node, comment) {
+          var type = comment.type;
+          if (type == "comment2") {
+            return /reuse|follow/i.test(comment.value);
+          }
+        }
+      }
+    })
+  ]
 };
