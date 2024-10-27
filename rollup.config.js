e4ef328181feaaced6b15205044c1d881cc46cf3
Gautam <i@velusgautam.com>
2024-10-27 22:31:37 +0100

    build: Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,32 @@
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import { version } from "./package.json";
 
 export default {
   input: "./src/main.js",
-  output: {
-    file: "dist/main.js",
-    format: "esm",
-    banner: "/* reuse-utility Library version: " + version + " */",
-    footer: "/* follow me on Github! https://github.com/velusgautam */"
-  },
+  output: [
+    {
+      file: "dist/main.esm.js",
+      format: "esm",
+      banner: "/* reuse-utility Library version: " + version + " */",
+      footer: "/* follow me on Github! https://github.com/velusgautam */",
+    },
+    {
+      file: "dist/main.cjs.js",
+      format: "cjs",
+      banner: "/* reuse-utility Library version: " + version + " */",
+      footer: "/* follow me on Github! https://github.com/velusgautam */",
+    },
+  ],
   plugins: [
     terser({
       output: {
-        comments: function(node, comment) {
+        comments: function (node, comment) {
           var type = comment.type;
           if (type == "comment2") {
             return /reuse|follow/i.test(comment.value);
           }
-        }
-      }
-    })
-  ]
+        },
+      },
+    }),
+  ],
 };
